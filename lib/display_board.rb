4203f8b68c18dcd1b76d4114163f2161ec25e599
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-29 04:25:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,7 @@
 # out the current state.
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
 puts "#{board[0]} | #{board[1]} | #{board[2]}"
 puts "-----------"
@@ -9,5 +10,3 @@ puts "#{board[3} | #{board[4]} | #{board[5]}"
 puts "-----------"
 puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
-
-display_board(board)
