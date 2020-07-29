d2c4a78816eaf9833247f12d76868abeb54af0f0
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-29 04:13:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,9 +6,9 @@ row = "   |   |   "
 separator = "-----------"
 
 def display_board(board)
-  puts row
-  puts separator
-  puts row
-  puts separator
-  puts row
+  puts "#{row}"
+  puts "#{separator}"
+  puts "#{row}"
+  puts "#{separator}"
+  puts "#{row}"
 end
